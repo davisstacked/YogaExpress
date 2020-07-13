01bb9ac448d73daec0855ec2866cf340910e8b0a
davisstacked <brettzvi@gmail.com>
2020-07-13 14:13:33 -0700

    database complete

--- models/Pose.js
+++ models/Pose.js
@@ -68,28 +68,24 @@ const poseDatabase = [
         img: "../public/images/legsUpTheWallPose.png"
     },
     {
-        // 10th pose
         name: "Locust Pose",
         benefits: ["improves posture", "helps relieve stress", "stretches the shoulders, chest, belly and thighs"],
         description: "preps beginners for deeper backbends, strengthening the back of the torso, legs and arms",
         img: "../public/images/locustPose.png"          
     },
     {
-        // 11th pose
         name: "Reclining Hand-to-Big-Toe Pose",
         benefits:["impoves digestion", "strengthens the knees", "stretches hips, thighs, hamstrings, groins and calves"],
         description: "the aching back, tight hamstring cure",
         img: "../public/images/recliningHandToBigToePose.png"
     },
     {
-        // 12th pose
         name: "Plow Pose",
         benefits:["reduces stess and fatigue", "stimulates the abdominal organs and the thyroid gland"],
         description: "reduces backache and helps you get to sleep",
         img: "../public/images/plowPose.png"
     },
     { 
-        // 13th pose
         name: "Revolved Side Angle Pose",
         benefits:["improves balance", "increases stamina", "improves digestion and aids elimination"],
         description: "requires a lot of flexibility to twist so deeply and ground the back heel",
@@ -97,49 +93,48 @@ const poseDatabase = [
 
     },
     {
-        // 14th pose
         name: "Half Lord of the Fishes Pose",
         benefits:["energizes the spine", "stimulates the liver and kidneys", "stretches the shoulders, hips, and neck"],
         description: "energizes the spine and stimulates the digestive fire",
         img: "../public/images/halfLordOfTheFishesPose.png"
     },
     {
-        // 15th pose
         name: "Extended Triangle Pose",
         benefits: ["improves digestion", "helps relieve stress", "stimulates the abdominal organs"],
         description: "the quintessential standing pose in many styles of yoga",
         img: "../public/images/extendedTrianglePose.png"
     },
     {
-        // 16th pose
         name: "Fish Pose",
         benefits: ["improves posture", "strengthens the muscles of the upper back and back of the neck"],
         description: "a traditional text calls fish pose the 'destroyer of all diseases",
         img: "../public/images/fishPose.png"
     },
     {
-        // 17th pose
         name: "Half Moon Pose",
         benefits: ["improves digestion", "helps relieve stress", "improves coordination and sense of balance"],
         description: "create leg and ankle strength as you seek stability and extend into this balancing pose",
         img: "../public/images/halfMoonPose.png"
     },
     {
-        // 18th pose
         name: "Marichi's Pose",
         benefits: ["stimulates the brain", "stretches the shoulders", "relieves mild backache and hip pain"],
         description: "sometimes called the 'Sage's Pose', Marichi's Pose is a wise addition to any practice",
         img: "../public/images/marichisPose.png"
     },
     {
-        // 19th pose
         name: "Revolved Head-to-Knee Pose",
-        benefits: [""],
+        benefits: ["improves digestion", "stretches the spine, shoulders and hamstrings"],
         description: "this twisting variation, takes the classic forward bend to the next level of release",
         img: "../public/images/revolvedHeadToKneePose.png"
+    },
+    {
+        name: "Revolved Triange Pose",
+        benefits: ["relieves mild back pain", "stretches the hips and spine", "opens the chest to improve breathing"],
+        description: "preparation for seated forward bends and twists, this pose is key to a skilled practice",
+        img: "../public/images/revolvedTrianglePose.png"
     }
-
-]
+];
 
 
 
